5c3542a346a1b7be87ef7ea984ea402092539476
NYHolzer <nissan.holzer@gmail.com>
2019-06-02 15:11:59 -0400

    Done.

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,5 +1,17 @@
 class Student < ActiveRecord::Base
   def to_s
-    self.first_name + " " + self.last_name
+    self.convert_nil
+  end
+
+  def convert_nil
+    if first_name == nil && last_name == nil 
+      " "
+    elsif first_name == nil && last_name != nil
+      last_name
+    elsif first_name != nil && last_name == nil
+      first_name
+    else 
+      self.first_name + " " + self.last_name
+    end
   end
 end
